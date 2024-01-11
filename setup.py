c3aee76fe9b6d8364d491663775f7d574fd24320
Tim Warner <timothywarner316@gmail.com>
2024-01-11 08:09:19 -0600

    test package workfow

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_world_package",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple 'hello world' Python package",
+    long_description="For use in the Microsoft Press course 'GitHub Actions Cert Prep'",
+    author="Tim Warner",
+    author_email="tim@timw.info"
+    # Add other parameters as needed
+)
